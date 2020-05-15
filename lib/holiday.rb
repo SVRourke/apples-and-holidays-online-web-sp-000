172423c9b1e3da797c939ba8e7b2b7713a9bc542
Sam Rourke <samuelvanrourke@gmail.com>
2020-05-15 03:52:52 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holidays = holiday_hash.each do |season, hash|
     puts "#{season.to_s.capitalize}:"
-    puts "#{{hash}}"
+    puts hash
     #
     # hash.each do |holiday, supplies|
     #   event = holiday.to_s.split("_").collect {|element| element.capitalize}.join(" ")
